08b491f2ef4b05e07038797caa77b4b45e146bfb
SebastianBuftea <sebibuftea85@gmail.com>
2024-01-08 18:18:58 +0100

    [add] header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script>
+import HeaderApp from './components/HeaderApp.vue';
 export default {
+  components: {
+    HeaderApp
+  }
 
 }
 </script>
 <template lang="">
   <div>
-    
+    <HeaderApp />
   </div>
 </template>
 
-<style lang="">
-  
+<style lang="scss">
+@use './styles/partials/variables' as *;
+@use './styles/generals.scss' as *;
 </style>
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,13 +1,93 @@
-<template lang="">
-    <div>
-        
-    </div>
-</template>
 <script>
 export default {
+    name: 'HeaderApp',
+    data() {
+        return {
+            links: [
+                {
+                    label: 'characters',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    label: 'comics',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    label: 'movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    label: 'tv',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    label: 'games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    label: 'collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    label: 'videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    label: 'fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    label: 'news',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    label: 'shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+
+        }
+    },
 
 }
 </script>
-<style lang="">
-    
+<template lang="">
+
+    <header>
+        <img src="../assets/vue-dc-comics-1/img/dc-logo.png" alt="dc_logo">
+        <nav>
+          <ul>
+            <li v-for="(link, index) in links" :key="index">
+                <a :class="link.current ? 'active' : ''" :href="link.url">
+                    {{link.label}}
+                </a>
+            </li>
+          </ul>
+        </nav>
+    </header>
+
+</template>
+
+<style lang="scss">
+header {
+    display: flex;
+    justify-content: space-between;
+    margin: 10px 100px;
+
+    ul {
+        list-style: none;
+
+    }
+
+}
 </style>
